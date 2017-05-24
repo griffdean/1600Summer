1eddb82e6d869bdc373ca86ba6740f3ab31e4647
Unknown <10820413@my.uvu.edu>
2017-05-24 11:34:53 -0600

    Adding if Statments in Unity

--- 1600Summer/Assets/Animal.cs
+++ 1600Summer/Assets/Animal.cs
@@ -6,21 +6,52 @@ public class Animal : MonoBehaviour {
     //Properties (things they are or have)
     //BodySegments
     public string[] bodySegments = { "Head", "Abs", "Thorax" };
+    public string animalName = "Jenny";
+
     //Health
     public int health = 100;
     //Speed
     public float speed = 0.0f;
     //Size
     public string size = "Big";
+    public bool canEat = false;
 
     public Color baseColor;
 
     //Functions (things they do)
-	//Breathe
+    //Breathe
+    public bool canBreathe = false;
     //Reproduce
     //Eat
+    void Start()
+    {
+        /*if (canEat)
+        {
+            print(this.name + " can eat.");
+        }
+        else
+        {
+            print(this.name + " can't eat.");
+        }*/
+
+        if (15 / 4 == 3)
+        {
+            print("3");
+        }
+
+        if (canBreathe)
+        {
+            print(this.name + " can breathe.");
+        }
+
+        if (animalName == "Griffon")
+        {
+            print("Griffon");
+        }
+    }
     //Move
     //Die
     //Grow
+
 
 }
